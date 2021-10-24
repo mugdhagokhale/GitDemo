dbaafebb0179b6c753d5afd516749242062fafd6
mugdha <mugdhagokhale4@gmail.com>
2021-10-24 16:45:30 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,14 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void Nativeappandroid() {
+		System.out.println("nativeapp");
+	}
+	@Test
+	public void IOSapp() {
+		System.out.println("iosapp");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void Browserautomation() {
+		System.out.println("browserautomation");
+	}
+	
+	@Test
+	public void elements() {
+		System.out.println("elements");
+	}
+	
+}
